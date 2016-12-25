70ac15b46a7c312de26194ea6684cf32c8833fe3
Vladislav Lewin <vlewin@suse.de>
2016-12-25 12:24:43 +0100

    Init commit

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+tput setaf 4; echo "*** Iterm2 setup"
+brew install Caskroom/cask/iterm2
+
+ITERM_SHELL_INTEGRATION=.iterm2_shell_integration.zsh
+
+if [ -f $ITERM_SHELL_INTEGRATION ]; then
+  tput setaf 3; echo "<< Skip iTerms2 shell integration installation"
+else
+  tput setaf 2; echo ">> Install iTerm shell integration"
+  curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
+fi
+
+tput setaf 4; echo "*** ZSH setup"
+brew install zsh zsh-completions
+brew install zsh-syntax-highlighting
+
+OH_MY_ZSH=.oh-my-zsh
+if [ -d $OH_MY_ZSH ]; then
+  tput setaf 3; echo "<< Skip Oh my ZSH installation"
+else
+  tput setaf 2; echo ">> Install oh-my-zsh"
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
+
+tput setaf 4; echo "*** .inputrc setup"
+INPUTRC=~/.inputrc
+if [ -f $INPUTRC ]; then
+  tput setaf 3; echo "<< Skip $INPUTRC setup"
+else
+tput setaf 2; echo ">> Install .inputrc modifications"
+cat <<-EOT >> $INPUTRC
+"\e[A": history-search-backward
+"\e[B": history-search-forward
+"\e[C": forward-char
+"\e[D": backward-char
+set show-all-if-ambiguous on
+set completion-ignore-case on
+TAB: menu-complete
+EOT
+fi
+
+tput setaf 4; echo "*** Atom setup"
+brew install  Caskroom/cask/atom
+
+apm install autocomplete-paths language-vue pigments vue-autocomplete
+
+tput setaf 4; echo "*** Additional applications"
+brew install Caskroom/cask/liteicon
+brew install Caskroom/cask/github-desktop
