29f24be1ddc24436bf669adee7716583be2fe99b
Vladislav Lewin <vlewin@suse.de>
2016-12-25 12:36:02 +0100

    Add system optimizations

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Application setup
 tput setaf 4; echo "*** Iterm2 setup"
 brew install Caskroom/cask/iterm2
 
@@ -49,3 +51,27 @@ apm install autocomplete-paths language-vue pigments vue-autocomplete
 tput setaf 4; echo "*** Additional applications"
 brew install Caskroom/cask/liteicon
 brew install Caskroom/cask/github-desktop
+
+# System optimizations
+tput setaf 4; echo '*** System optimizations'
+tput setaf 2; echo '>> Speed Up Animation Speed of Hiding & Displaying the Mac OS X Dock'
+defaults write com.apple.dock autohide-time-modifier -float 0.12
+# defaults delete com.apple.dock autohide-time-modifier
+
+tput setaf 2; echo '>> Speed Up Mission Control Animations'
+defaults write com.apple.dock expose-animation-duration -float 0.15
+# defaults delete com.apple.dock expose-animation-duration
+
+tput setaf 2; echo '>> Disable Spaces Animation'
+defaults write com.apple.dock workspaces-swoosh-animation-off -bool YES
+# defaults write com.apple.dock workspaces-swoosh-animation-off -bool NO
+
+tput setaf 2; echo '>> Restart Dock ...'
+killall Dock
+
+tput setaf 2; echo '>> Unload Mac OS deamons'
+tput setaf 7;
+launchctl unload /System/Library/LaunchAgents/com.apple.gamed.plist
+launchctl unload /System/Library/LaunchAgents/com.apple.PhotoLibraryMigrationUtility.XPC.plist
+launchctl unload /System/Library/LaunchAgents/com.apple.cloudphotosd.plist
+launchctl unload /System/Library/LaunchAgents/com.apple.icloud.fmfd.plist
